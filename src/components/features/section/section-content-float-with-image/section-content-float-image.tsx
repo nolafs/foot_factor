c2a9d00b208590ce3df4f61199d835bdb629153b
Olaf Siebert <olaf@biffnewmedia.com>
2025-04-16 13:08:32 +0200

    feat: Section
    - full size image
    - with floating text

--- src/components/features/section/section-content-float-with-image/section-content-float-image.tsx
+++ src/components/features/section/section-content-float-with-image/section-content-float-image.tsx
@@ -23,13 +23,14 @@ interface SectionDefaultProps {
 export const SectionContentImageFloatText = ({as = 'section',heading, body, image, links, hasBooking, bookingLabel, variation, slice_type}: SectionDefaultProps) => {
   const Tag = as; //
     return (
-      <Tag data-slice-type={slice_type} data-slice-variation={variation} className={cn('w-full isolated relative')}>
+      <Tag data-slice-type={slice_type} data-slice-variation={variation} className={cn('relative w-full max-h-[1440px] isolated')}>
 
             <div className={'w-full flex justify-center'}>
               <PrismicNextImage field={image}
                                 className={cn('w-full h-full object-center object-cover')}/>
             </div>
         <div className={'absolute top-0 left-0 z-1 w-full h-1/2 bg-gradient-to-t from-stone-50/0 to-white'}/>
+        <div className={'absolute top-0 left-0 z-1 w-full h-full bg-gradient-to-l from-stone-50/0 to-white'}/>
         <div className={'absolute z-10 top-0 left-0 w-full'}>
           <Container className={'lg:py-28 py-16 md:py-24'}>
               <div className={'flex flex-col w-full md:w-6/12 lg:w-5/12  pt-16 md:pt-24 lg:py-32'}>
