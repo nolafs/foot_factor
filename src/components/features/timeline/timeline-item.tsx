a08f34bce71b889adecf9af72aac249be25b4736
Olaf Siebert <olaf@biffnewmedia.com>
2025-07-02 14:59:13 +0200

    feat: Timeline block mobile

--- src/components/features/timeline/timeline-item.tsx
+++ src/components/features/timeline/timeline-item.tsx
@@ -41,7 +41,7 @@ export const TimelineListItem = ({data, isEven = false}: TimelineListItemProps) 
               onAnimationComplete={handleAnimationComplete}
           >
 
-              <div ref={triggerRef} className={cn('flex flex-col items-start gap-4  ', isEven ? 'md:pr-10 md:items-end' : 'md:pl-10 md:items-start')}>
+              <div ref={triggerRef} className={cn('flex flex-col items-start gap-4', isEven ? 'md:pr-10 md:items-end' : 'md:pl-10 md:items-start')}>
                 {data.image && (
                     <figure className={'aspect-w-16 aspect-h-9 w-full overflow-hidden rounded-2xl'}>
                       <PrismicNextImage
@@ -60,7 +60,7 @@ export const TimelineListItem = ({data, isEven = false}: TimelineListItemProps) 
                 <div className={cn('relative flex flex-col md:flex-row gap-5 mt-5', isEven && 'md:flex-row-reverse')}>
 
                   <motion.div initial={{opacity: 0, width: 0}} animate={lineControl}
-                      className={cn("absolute z-10 top-5 h-[2px] w-[14px] bg-secondary/50 -left-10 -translate-x-1/2", isEven ? 'md:-right-10 md:translate-x-1/2' : 'md:-left-10 md:-translate-x-1/2' )}/>
+                      className={cn("absolute z-10 top-5 h-[2px] w-[14px] bg-secondary/50 ", isEven ? '-left-10 -translate-x-1/2 md:left-auto  md:!-right-10 md:translate-x-1/2' : 'md:-left-10 md:-translate-x-1/2' )}/>
 
                   <div className={'text-5xl font-heading text-accent'}>
                     {data.year}
