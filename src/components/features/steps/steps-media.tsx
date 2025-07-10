7b6b43daad1def912835b7af67dea157dcc5a3d8
Olaf Siebert <olaf@biffnewmedia.com>
2025-07-10 15:48:35 +0200

    feat: Media Stepper

--- src/components/features/steps/steps-media.tsx
+++ src/components/features/steps/steps-media.tsx
@@ -4,13 +4,20 @@ import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import {useGSAP} from '@gsap/react';
 import {Container} from '@/components/ui/container';
+import {VerticalStepsWithImagesSliceVideoVerticalStepperPrimary} from '@/prismic-types';
+import {PrismicNextImage} from '@prismicio/next';
+import cn from 'clsx';
+import {isFilled} from '@prismicio/client';
+import {Body, Heading} from '@/components/ui/text';
+import {PrismicRichText} from '@prismicio/react';
+import ButtonRow from '@/components/ui/button-row';
 
 if (typeof window !== 'undefined') {
     gsap.registerPlugin(ScrollTrigger);
 }
 
 interface StepsMediaProps {
-    data: any[]
+    data: VerticalStepsWithImagesSliceVideoVerticalStepperPrimary;
 }
 
 export const StepsMedia = ({data}: StepsMediaProps) => {
@@ -28,6 +35,29 @@ export const StepsMedia = ({data}: StepsMediaProps) => {
       <Container>
           <div ref={contentRef} className={'relative w-full isolate'}>
               <ul ref={listRef} className={'w-full flex flex-col'}>
+                <li>
+                  <div className={'relative w-full h-full max-h-svh isolated overflow-hidden'}>
+                    <div className={'w-full h-full flex justify-center'}>
+                      <PrismicNextImage field={data.intro_image}
+                                        className={cn('w-full h-full object-center object-cover')}/>
+                    </div>
+                    <div className={'absolute z-10 top-0 left-0 w-full'}>
+
+                        <div
+                            className={cn('flex flex-col w-full h-full justify-center items-center')}>
+                          <div
+                              className={cn('flex flex-col w-full md:w-6/12 lg:w-6/12 xl:w-5/12  pt-16 md:pt-24 lg:py-32')}>
+                            {isFilled.richText(data.intro_body) && (
+                                <Heading as={'h2'} primary={true} className={'content-master text-animation'}>
+                                  <PrismicRichText field={data.intro_body}/>
+                                </Heading>
+                            )}
+                          </div>
+                        </div>
+
+                    </div>
+                  </div>
+                </li>
               </ul>
           </div>
       </Container>
--- src/slices/VerticalStepsWithImages/index.tsx
+++ src/slices/VerticalStepsWithImages/index.tsx
@@ -2,6 +2,7 @@ import { type FC } from 'react';
 import { type Content } from '@prismicio/client';
 import { type SliceComponentProps } from '@prismicio/react';
 import Steps from '@/components/features/steps/steps';
+import StepsMedia from '@/components/features/steps/steps-media';
 
 /**
  * Props for `VerticalStepsWithImages`.
@@ -12,6 +13,15 @@ export type VerticalStepsWithImagesProps = SliceComponentProps<Content.VerticalS
  * Component for "VerticalStepsWithImages" Slices.
  */
 const VerticalStepsWithImages: FC<VerticalStepsWithImagesProps> = ({ slice }) => {
+
+  if (slice.variation === 'videoVerticalStepper') {
+    return <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+      <StepsMedia data={slice.primary}/>
+    </section> // Return empty if the variation does not match
+
+  }
+
+
   return (
     <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
       <Steps data={slice.primary.steps} />
