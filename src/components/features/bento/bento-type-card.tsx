b581421d328b580494e7af1a7cfd1e8eaea52f66
Olaf Siebert <olaf@biffnewmedia.com>
2025-07-18 12:08:53 +0200

    feat: edit search

--- src/components/features/bento/bento-type-card.tsx
+++ src/components/features/bento/bento-type-card.tsx
@@ -20,46 +20,46 @@ export const BentoTypeCard = ({item}: BentoTypeCardProps) => {
 
 
     if(item.card_type === '1') {
-        return (
-            <>
-            <motion.div
-                ref={parallax.ref}
-                style={{
-                    y: parallax.y,
-                    scale: parallax.scale
-                }}
-                className="absolute inset-0 z-10"
-            >
-                <PrismicNextImage field={item.image} className={cn('absolute md:relative w-full h-full object-cover')}/>
-            </motion.div>
+        return (isFilled.link(item.link) && <div className={'group/item'}>
+            <PrismicNextLink field={item.link} >
+                <motion.div
+                    ref={parallax.ref}
+                    style={{
+                        y: parallax.y,
+                        scale: parallax.scale
+                    }}
+                    className="absolute inset-0 z-10"
+                >
+                    <PrismicNextImage field={item.image} className={cn('absolute md:relative w-full h-full object-cover transition delay-150 duration-300 ease-in-out group-hover/item:scale-105')}/>
+                </motion.div>
                 <div
-                    className="absolute inset-0 rounded-lg bg-gradient-to-t from-primary-950/90 to-transparent max-lg:rounded-4xl lg:rounded-4xl overflow-hidden"/>
+                    className="absolute inset-0 rounded-lg bg-gradient-to-t from-primary-950/70 to-transparent max-lg:rounded-4xl lg:rounded-4xl overflow-hidden"/>
                 <div className="relative mt-32 md:mt-0 md:absolute  z-5 bottom-0 p-7 md:p-10 lg:p-16  z-10 w-full">
-                    <Heading as={'h3'} color={'White'}>{item.heading}</Heading>
+                    <Heading as={'h3'} color={'White'} className={'group-hover/item:text-accent'}>{item.heading}</Heading>
                     <Lead className="mt-2 max-w-2xl" color={'Light'} size={'sm'}>
                         {item.lead}
                     </Lead>
-                    {isFilled.link(item.link) && (<div className={'flex justify-end'}>
-                            <PrismicNextLink field={item.link}
-                                             className={cn(buttonVariants({variant: 'default', size: 'icon'}))}>
-                                <ArrowRight className={'h-4 w-4'} strokeWidth={4}/>
-                            </PrismicNextLink>
+                    <div className={'flex justify-end'}>
+                            <div className={cn('bg-accent rounded-full h-12 w-12 flex justify-center items-center text-neutral-50 group-hover/item:bg-primary group-hover/item:text-accent')}>
+                                <ArrowRight size={22} strokeWidth={4}/>
+                            </div>
                         </div>
-                    )}
+
                 </div>
-            </>
+            </PrismicNextLink>
+            </div>
         )
     }
 
     if (item.card_type === '2') {
-        return (
-            <>
+        return (isFilled.link(item.link) && <div className={'group/item'}>
+            <PrismicNextLink field={item.link}>
                 <div className="p-7 sm:p-7 md:p-10 lg:p-16 text-center">
                     {item.image_position === 'top' && (<div className={'w-full h-auto  mb-5'}>
                             <PrismicNextImage field={item.image} className={cn('w-full h-full object-cover')}/>
                         </div>
                     )}
-                    <Heading as={'h3'} className="text-secondary-950">{item.heading}</Heading>
+                    <Heading as={'h3'} className="text-secondary-950 group-hover/item:text-accent">{item.heading}</Heading>
                     <Lead className="mt-2 max-w-2xl text-lg" size={'sm'}>
                         {item.lead}
                     </Lead>
@@ -70,14 +70,15 @@ export const BentoTypeCard = ({item}: BentoTypeCardProps) => {
                     </div>
                 )}
 
-                {isFilled.link(item.link) && (
-                    <div className={'relative z-10 md:absolute bottom-0 w-full p-7 md:p-10 lg:p-16 flex justify-end'}>
-                        <PrismicNextLink field={item.link}
-                                         className={cn(buttonVariants({variant: 'default', size: 'icon'}))}>
-                            <ArrowRight size={32} strokeWidth={4}/>
-                        </PrismicNextLink>
-                    </div>)}
-            </>
+
+                <div className={'relative z-10 md:absolute bottom-0 w-full p-7 md:p-10 lg:p-16 flex justify-end'}>
+                    <div
+                        className={cn('bg-accent rounded-full h-12 w-12 flex justify-center items-center text-neutral-50 group-hover/item:bg-primary group-hover/item:text-accent')}>
+                        <ArrowRight size={22} strokeWidth={4}/>
+                    </div>
+                </div>
+            </PrismicNextLink>
+            </div>
         )
     }
 }
