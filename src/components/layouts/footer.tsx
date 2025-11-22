51424ce200f70ab7013f6a6951e5ad97ee52125a
Olaf Siebert <olaf@biffnewmedia.com>
2025-11-22 10:26:02 +0100

    fix: no h tag in footer

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -44,9 +44,9 @@ export function Footer({ navigation, settings }: FooterProps) {
               <div className="mt-10 grid grid-cols-2 gap-y-10 pb-10 lg:grid-cols-6 lg:gap-8">
                 <div className="col-span-2 flex flex-col justify-between">
                   <Link href="/" className={'grow'} >
-                    <h2 id="footer-heading" className="sr-only">
+                    <span id="footer-heading" className="sr-only">
                       Foot Factor
-                    </h2>
+                    </span>
                     <PrismicNextImage field={settings.footer_logo} className="inline !max-w-[250px]" />
                   </Link>
                 </div>
