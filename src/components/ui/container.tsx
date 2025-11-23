76aa99e1f4988d0346e5af0b156de9cb25f61a35
Olaf Siebert <olaf@biffnewmedia.com>
2025-11-23 18:52:52 +0100

    fix: add address to footer

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -1,45 +1,38 @@
-import {cn} from '@/lib/utils';
-import React, {
-  forwardRef,
-  type ElementType,
-  type ComponentPropsWithRef,
-  type ReactNode
-} from 'react';
-import {Wave} from '@/components/wave';
+import { cn } from '@/lib/utils';
+import React, { forwardRef, type ElementType, type ComponentPropsWithRef, type ReactNode } from 'react';
+import { Wave } from '@/components/wave';
+import { WaveType } from '@/components/features/hero/hero-simple';
 
 type ContainerProps<T extends ElementType> = {
   as?: T;
   className?: string;
   children?: ReactNode;
   padding?: 'sm' | 'md' | 'base' | 'lg';
-  color?:  string;
+  color?: string;
   wave?: string;
   fullWidth?: boolean;
-} & ComponentPropsWithRef<T>;
+} & Omit<ComponentPropsWithRef<T>, 'as'>;
 
 type PolymorphicContainer = <T extends ElementType = 'div'>(
-    props: ContainerProps<T> & {
-      ref?: React.Ref<React.ElementRef<T>>
-    }
+  props: ContainerProps<T> & {
+    ref?: React.Ref<Element>;
+  },
 ) => React.ReactElement | null;
 
-const ContainerComponent = forwardRef<
-    HTMLDivElement,
-    ContainerProps<ElementType>
->(function Container<T extends ElementType = 'div'>(
-    {
-      as,
-      className,
-      children,
-      padding = 'base',
-      color = 'transparent',
-      wave,
-      fullWidth = false,
-      ...rest
-    }: ContainerProps<T>,
-    ref: React.Ref<React.ElementRef<T>>
+const ContainerComponent = forwardRef(function Container<T extends ElementType = 'div'>(
+  {
+    as,
+    className,
+    children,
+    padding = 'base',
+    color = 'transparent',
+    wave,
+    fullWidth = false,
+    ...rest
+  }: ContainerProps<T>,
+  ref: React.Ref<Element>,
 ) {
-  const Component = as ?? 'div';
+  const Component = (as ?? 'div') as ElementType;
   let backgroundColorClass = '';
   const colorLowerCase = color.toLowerCase();
 
@@ -61,28 +54,28 @@ const ContainerComponent = forwardRef<
       break;
     default:
       backgroundColorClass = 'bg-transparent';
-      break
+      break;
   }
 
   return (
-      <Component
-          ref={ref}
-          className={cn('relative w-full mx-auto overflow-hidden z-10',
-              className,
-              backgroundColorClass,
-              padding === 'base' && (!fullWidth && 'px-6 lg:px-8'),
-              padding === 'md' && (!fullWidth ? 'px-6 lg:px-8 py-10 md:py-16 lg:py-20' : 'py-10 md:py-16 lg:py-20'),
-              padding === 'lg' && (!fullWidth ? 'px-6 lg:px-8 py-16 md:py-24 lg:py-28' : 'py-16 md:py-24 lg:py-28'),
-          )}
-          {...rest}
-      >
-        {wave && (
-            <div className={'absolute inset-0 w-full h-full overflow-hidden'}>
-              <Wave waveType={wave} />
-            </div>
-        )}
-        <div className={cn("relative w-full z-20", !fullWidth && 'container mx-auto ')}>{children}</div>
-      </Component>
+    <Component
+      ref={ref}
+      className={cn(
+        'relative z-10 mx-auto w-full overflow-hidden',
+        className,
+        backgroundColorClass,
+        padding === 'base' && !fullWidth && 'px-6 lg:px-8',
+        padding === 'md' && (!fullWidth ? 'px-6 py-10 md:py-16 lg:px-8 lg:py-20' : 'py-10 md:py-16 lg:py-20'),
+        padding === 'lg' && (!fullWidth ? 'px-6 py-16 md:py-24 lg:px-8 lg:py-28' : 'py-16 md:py-24 lg:py-28'),
+      )}
+      {...rest}>
+      {wave && (
+        <div className={'absolute inset-0 h-full w-full overflow-hidden'}>
+          <Wave waveType={wave as WaveType} />
+        </div>
+      )}
+      <div className={cn('relative z-20 w-full', !fullWidth && 'container mx-auto')}>{children}</div>
+    </Component>
   );
 });
 
@@ -90,4 +83,4 @@ ContainerComponent.displayName = 'Container';
 
 const Container = ContainerComponent as PolymorphicContainer;
 
-export {Container};
+export { Container };
